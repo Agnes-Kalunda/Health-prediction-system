393651abc652edce4e25c57f2eda5ba7cbe3494f
Agnes <agypeter97@gmail.com>
2024-09-01 23:09:43 +0300

    HealthData model

--- backend/backendApp/models.py
+++ backend/backendApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class HealthData(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    age = models.IntegerField()
+    weight=models.FloatField()
+    height=models.FloatField()
+    blood_pressure=models.CharField(max_length=10)
+    heart_rate=models.IntegerField()
+    activity_level=models.CharField(max_length=50)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.username} - {self.timestamp}"
