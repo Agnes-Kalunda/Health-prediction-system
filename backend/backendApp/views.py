5ebb14f86fabf4f9881d0c39370d1b08f61468ba
Agnes <agypeter97@gmail.com>
2024-09-01 23:17:16 +0300

    HealthDataViewSet view

--- backend/backendApp/views.py
+++ backend/backendApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import HealthData
+from .serializers import HealthDataSerializer
 
 # Create your views here.
+
+class HealthDataViewSet(viewsets.ModelViewSet):
+    queryset = HealthData.objects.all()
+    serializer_class = HealthDataSerializer
