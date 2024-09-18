c6b089baafd70b881526f8c3bc083180d82a598b
Agnes <agypeter97@gmail.com>
2024-09-18 16:23:04 +0300

    map heart-disease-pred view to urls

--- backend/backendApp/urls.py
+++ backend/backendApp/urls.py
@@ -1,9 +1,11 @@
+# urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import KidneyPredictionViewSet
+from .views import KidneyPredictionViewSet, HeartDiseasePredictionViewSet
 
 router = DefaultRouter()
 router.register(r'kidney-predictions', KidneyPredictionViewSet)
+router.register(r'heart-disease-predictions', HeartDiseasePredictionViewSet, basename='heart-disease-prediction')
 
 urlpatterns = [
     path('', include(router.urls)),
